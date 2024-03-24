eb03509851fcefe89b404c785d2e94a9ac8dd8e5
Ghassen <ghassen.rafraf@eniso.u-sousse.tn>
2024-03-24 06:11:54 +0100

    modified:   controllers/employeeControllers.js

--- controllers/employeeControllers.js
+++ controllers/employeeControllers.js
@@ -1,15 +1,13 @@
 const Employee = require("../models/employeeModel");
 
+
+//add member controller
 const addMember = async (req, res) => {
-    try{
+
     const { firstName, lastName, email, contact, clearanceLevel, department } = req.body;
     const employeeModel =   await Employee.create({ firstName, lastName, email, contact, clearanceLevel, department });
     res.json({employeeModel});
 
-    res.status(201).json({ message: "Employee added successfully" });
-    } catch (error) {
-        res.status(400).json({ message: "Server error" });
-    }
 };
 
 module.exports = { addMember };
