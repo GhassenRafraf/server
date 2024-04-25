901aa63b44236271b140bb1b009e7fece7f33f2c
Ghassen <ghassen.rafraf@eniso.u-sousse.tn>
2024-04-25 01:53:36 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -9,16 +9,51 @@ const employeeControllers = require("./controllers/employeeControllers");
 const request = require("./controllers/accessRequest");
 const { initializeMQTT } = require("./config/connectToMQTT");
 const http = require("http");
+const MQTT_TOPIC = "test";
+const WebSocket = require("ws");
+
 const app = express();
 const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 app.use(cors());
 app.use(express.json());
 
 connectToDb();
+
 // Initialize MQTT connection
 const mqttClient = initializeMQTT();
 employeeControllers.setMQTTClient(mqttClient);
 
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    const decodedMessage = message.toString('utf-8');
+    console.log("Received message from client:", decodedMessage);  });
+    
+  ws.send("Welcome! You are now connected to the WebSocket server.");
+});
+
+// Update latestData and broadcast to WebSocket clients when a new MQTT message is received
+mqttClient.on("message", function (topic, message) {
+  console.log("Received message from MQTT:", message.toString());
+  const newMessage = message.toString();
+
+  if (latestData !== newMessage) {
+    latestData = newMessage;
+    
+    // Send message to all WebSocket clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ data: latestData }));
+      }
+    });
+  }
+});
+
+// Handle WebSocket connection errors
+wss.on("error", (error) => {
+  console.error("WebSocket server error:", error);
+});
 
 // Routes
 app.post("/addMember", employeeControllers.addMember);
